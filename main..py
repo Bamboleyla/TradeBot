b680073760995726e8849bf8fa459917a1fdd720
Dmitriy V. <dvorobjevredstar@mail.ru>
2024-10-08 11:16:07 +0500

    prerare loging

--- main..py
+++ main..py
@@ -1,1 +1,39 @@
-print("Hello, World!")
+import logging
+import os
+
+from logging.handlers import RotatingFileHandler
+
+logger = logging.getLogger(__name__)
+
+
+def prepare_logs() -> None:
+    """Prepare logging system for the bot.
+
+    This function does the following:
+        - Ensure "logs/" directory exists in the current working directory.
+        - Set up basic configuration for the logging module.
+        - Configure a rotating file handler which logs to robot.log in the logs/ directory.
+    """
+    # Ensure "logs/" directory exists in the current working directory
+    if not os.path.exists("logs/"):
+        # Create "logs/" directory
+        os.makedirs("logs/")
+
+    # Set up basic configuration for the logging module
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s - %(module)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s",
+        handlers=[
+            RotatingFileHandler(
+                "logs/robot.log", maxBytes=100000000, backupCount=10, encoding="utf-8"
+            )
+        ],
+        encoding="utf-8",
+    )
+
+
+if __name__ == "__main__":
+    # Prepare logging system
+    prepare_logs()
+
+    logger.info("Program start")
