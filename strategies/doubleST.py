dba66d6adb53280597078932f6cc2cfacf2a3c58
DVorobyov <dvorobjevredstar@mail.ru>
2024-11-07 17:56:56 +0500

    create short

--- strategies/doubleST.py
+++ strategies/doubleST.py
@@ -49,6 +49,18 @@ class DoubleST:
 
     def long_take_profit(self, long_prise: float, open: float, close: float, high: float) -> bool:
         return True if open > long_prise + 1.5 or close > long_prise + 1.5 or high > long_prise + 1.5 else False
+    
+    def short_buy(self, open: float, close: float, hight:float, st3: float,st5: float) -> bool:
+        if st3 < st5:
+            if open < st3 and close < st3 and hight > st3:
+                return True
+        return False
+
+    def short_sell(self, open: float, close: float, st3: float) -> bool:
+        return True if open > st3 or close > st3 else False
+
+    def short_take_profit(self, short_prise: float, open: float, close: float, low: float) -> bool:
+        return True if open < short_prise - 20 or close < short_prise - 20 or low < short_prise - 20 else False
 
     def show(self) -> None:
         data = pd.read_csv(self.__export_data, header=0)
@@ -58,7 +70,7 @@ class DoubleST:
         stocks = 0  # amount of stocks
         action = None  # 'BUY' or 'SELL'
         last_buy = 0  # last price of buy
-        commission = 0.0005  # broker commission is 0,05%
+        commission = 0.0005  # broker commission is 0,05%        
 
         for index, row in data.iterrows():
             if index == 0:
@@ -68,6 +80,8 @@ class DoubleST:
                 continue
             elif index == len(data) - 1 and stocks > 0:
                 action = 'LONG_SELL'
+            elif index == len(data) - 1 and stocks < 0:
+                action = 'SHORT_SELL'
 
             if action == 'LONG_BUY' and stocks == 0:
                 data.loc[index, 'SIGNAL'] = action
@@ -89,13 +103,49 @@ class DoubleST:
                 data.loc[index, 'SELL_PRISE'] = row['open']
                 last_buy = 0
                 action = None
+            elif action == 'SHORT_BUY' and stocks == 0:
+                if row['open']<row['ST3'] :
+                    data.loc[index, 'SIGNAL'] = action
+                    account += row['open']*10
+                    account -= row['open']*10 * commission
+                    data.loc[index, 'Account'] = account
+                    stocks = -10
+                    data.loc[index, 'BUY_PRISE'] = row['open']
+                    last_buy = row['open']
+                    action = None
+                else:
+                    action = None
+            elif action == 'SHORT_SELL' or action == 'SHORT_TAKE_PROFIT' and stocks < 0:
+                data.loc[index, 'SIGNAL'] = action
+                account -= row['open']*10
+                account -= row['open']*10 * commission
+                data.loc[index, 'Account'] = account
+                data.loc[index, 'P/L'] = (row['close'] - last_buy) * 10
+                stocks = 0
+                data.loc[index, 'SELL_PRISE'] = row['open']
+                last_buy = 0
+                action = None
 
-            if stocks == 0 and self.long_buy(data.loc[index - 1], row):
-                action = 'LONG_BUY'
-            elif stocks > 0 and self.long_sell(row['open'], row['close'], row['ST3']):
-                action = 'LONG_SELL'
-            elif stocks > 0 and self.long_take_profit(last_buy, row['open'], row['close'], row['high']):
-                action = 'LONG_TAKE_PROFIT'
+            if stocks == 0:
+                if self.long_buy(data.loc[index - 1], row):
+                    action = 'LONG_BUY'
+                elif self.short_buy(row['open'], row['close'], row['high'], row['ST3'], row['ST5']):
+                    action = 'SHORT_BUY'
+
+            elif stocks > 0:
+                if self.long_sell(row['open'], row['close'], row['ST3']):
+                    action = 'LONG_SELL'
+                elif  self.long_take_profit(last_buy, row['open'], row['close'], row['high']):
+                    action = 'LONG_TAKE_PROFIT'
+
+            
+            if stocks < 0:
+                if self.short_sell(row['open'], row['close'], row['ST3']):
+                    action = 'SHORT_SELL'
+                elif  self.short_take_profit(last_buy, row['open'], row['close'], row['low']):
+                    action = 'SHORT_TAKE_PROFIT'
+            
+            
 
         print(f'Final account: {account}')
         print(f'Stocks: {stocks}')
@@ -119,11 +169,32 @@ class DoubleST:
         fplt.plot(data['EMA'], legend='EMA')
 
         fplt.plot(data['BUY_PRISE'], color='b', style='x', width=2)
-        fplt.plot(data.loc[data['SIGNAL'] == 'LONG_BUY', 'BUY_PRISE']-1, color='#4a5', style='^', legend='buy', width=2)
+
+        long_buy = data.loc[data['SIGNAL'] == 'LONG_BUY', 'BUY_PRISE']
+        if not long_buy.empty:
+            fplt.plot(long_buy-1, color='#4a5', style='^', legend='buy', width=2)
 
         fplt.plot(data['SELL_PRISE'], color='b', style='x', width=2)
-        fplt.plot(data.loc[data['SIGNAL'] == 'LONG_SELL', 'SELL_PRISE']+1, color='r', style='v', legend='long sell', width=2)
-        fplt.plot(data.loc[data['SIGNAL'] == 'LONG_TAKE_PROFIT', 'SELL_PRISE']+1, color='#4a5', style='p', legend='take profit', width=2)
+
+        long_sell = data.loc[data['SIGNAL'] == 'LONG_SELL', 'SELL_PRISE']
+        if not long_sell.empty:
+            fplt.plot(long_sell+1, color='r', style='v', legend='sell', width=2)
+        
+        long_take_profit = data.loc[data['SIGNAL'] == 'LONG_TAKE_PROFIT', 'SELL_PRISE']
+        if not long_take_profit.empty:
+            fplt.plot(long_take_profit+1, color='#4a5', style='p', legend='take profit', width=2)
+    
+        short_buy = data.loc[data['SIGNAL'] == 'SHORT_BUY', 'BUY_PRISE']
+        if not short_buy.empty:
+            fplt.plot(short_buy-1, color='#4a6', style='o', legend='short buy', width=2)
+  
+        short_sell = data.loc[data['SIGNAL'] == 'SHORT_SELL', 'SELL_PRISE']
+        if not short_sell.empty:
+            fplt.plot(short_sell+1, color='r', style='o', legend='short sell', width=2)
+        
+        short_take_profit = data.loc[data['SIGNAL'] == 'SHORT_TAKE_PROFIT', 'SELL_PRISE']
+        if not short_take_profit.empty:
+            fplt.plot(short_take_profit-1, color='r', style='p', legend='short take profit', width=2)
 
         fplt.add_legend('Double SuperTrend')
         fplt.show()
