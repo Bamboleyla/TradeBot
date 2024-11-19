8b2b35e4ccba536f19efb65bd01ba45886b573e9
DVorobyov <dvorobjevredstar@mail.ru>
2024-11-19 15:23:53 +0500

    add take profit

--- strategies/doubleST/main.py
+++ strategies/doubleST/main.py
@@ -56,6 +56,8 @@ class DoubleST:
                 continue
             elif index == len(data) - 1 and stocks > 0:
                 orders.append({'order': 'SELL_LIMIT', 'signal': 'LONG_SELL', 'price': row['open']})
+            elif 'TAKE_PROFIT' in data.columns and data.loc[index - 1, 'TAKE_PROFIT'] is not None:
+                data.loc[index, 'TAKE_PROFIT'] = data.loc[index - 1, 'TAKE_PROFIT']
 
                 # signals
             if stocks == 0:
@@ -77,6 +79,7 @@ class DoubleST:
                             stocks += 10
                             data.loc[index, 'SIGNAL'] = order['signal']
                             data.loc[index, 'BUY_PRICE'] = order['price']
+                            data.loc[index, 'TAKE_PROFIT'] = round(order['price'] + var_profit, 2)
                             orders.clear()
                     elif order['order'] == 'SELL_LIMIT':
                         if order['price'] <= row['high'] and order['price'] >= row['low']:
@@ -84,7 +87,14 @@ class DoubleST:
                             data.loc[index, 'SIGNAL'] = order['signal']
                             data.loc[index, 'SELL_PRICE'] = order['price']
                             orders.clear()
+                            data.loc[index, 'TAKE_PROFIT'] = None
 
+            if 'TAKE_PROFIT' in data.columns and data.loc[index, 'TAKE_PROFIT'] is not None:
+                if data.loc[index, 'TAKE_PROFIT'] <= row['high'] and data.loc[index, 'TAKE_PROFIT'] >= row['low']:
+                    stocks -= 10
+                    data.loc[index, 'SIGNAL'] = 'TAKE_PROFIT'
+                    data.loc[index, 'SELL_PRICE'] = data.loc[index, 'TAKE_PROFIT']
+                    data.loc[index, 'TAKE_PROFIT'] = None
         return data
 
     def report(self, data: pd.DataFrame) -> None:
@@ -121,12 +131,13 @@ class DoubleST:
 
         loss = len(deals[deals['P/L'] < 0])  # number of losses
         profit = len(deals[deals['P/L'] > 0])  # number of profits
-        win_rate = round((profit / (loss + profit)) * 100, 2)  # win rate
+        win_rate = str(round((profit / (loss + profit)) * 100, 2))+'%'  # win rate
         result = (account - init)/init*100  # result in %
 
         # report
-        print(f'|TRADES|LOSS|WIN|WIN RATE|ACCOUNT_START|ACCOUNT_END|RESULT|\n|  {trades}  | {loss}  | {
-              profit} |  {win_rate}% |    {init}     |  {round(account, 2)}  | {round(result, 2)} |')
+        report = pd.DataFrame(columns=['trades', 'loss', 'profit', 'win_rate', 'account_start', 'account_end', 'result'])
+        report.loc[len(report)] = [trades, loss, profit, win_rate, init, round(account, 2), str(round(result, 2))+'%']
+        print(report)
 
     def show(self, data: pd.DataFrame) -> None:
         self.report(data)
@@ -139,6 +150,7 @@ class DoubleST:
         fplt.plot(data['ST_FAST'], legend='ST_FAST', width=2)
         fplt.plot(data['ST_SLOW'], legend='ST_SLOW', width=2)
         fplt.plot(data['EMA'], legend='EMA')
+        fplt.plot(data['TAKE_PROFIT'], legend='TAKE_PROFIT', width=1, color='g')
 
         fplt.plot(data['BUY_PRICE'], color='b', style='x', width=2)
 
