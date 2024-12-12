e3fd31694fd847f9023ed8ab35964aa6b78972f4
DVorobyov <dvorobjevredstar@mail.ru>
2024-12-12 19:05:32 +0500

    change take profit

--- services/orders.py
+++ services/orders.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 from services.position import Position
+from datetime import datetime
 
 
 class Orders():
@@ -8,7 +9,6 @@ class Orders():
         self.__orders = []
         self.__order_list = pd.DataFrame()
         self.__position = position
-        self.__take_profit = None
 
     def create(self, order: dict):
         self.__orders.append(order)
@@ -23,30 +23,25 @@ class Orders():
                 elif order['order'] == 'SELL_LIMIT':
                     self.__sell_limit(order, row, index)
 
-            # take-profits
-        if self.__take_profit is not None:
-            if self.__take_profit <= row['high'] and self.__take_profit >= row['low']:
-                self.__position.decrease('WithDoubleTrend', 10)
-                self.__order_list.loc[index, 'SIGNAL'] = 'TAKE_PROFIT'
-                self.__order_list.loc[index, 'SELL_PRICE'] = self.__take_profit
-                self.__take_profit = None
-            else:
-                self.__order_list.loc[index, 'TAKE_PROFIT'] = self.__take_profit
+                elif order['order'] == 'TAKE_PROFIT':
+                    self.__take_profit(order, row, index)
 
         if self.__position.get_size() > 0 and pd.to_datetime(row['date']).time() == pd.Timestamp('23:45:00').time():
             self.__position.decrease('WithDoubleTrend', 10)
             self.__order_list.loc[index, 'SIGNAL'] = 'MARKET_STOP'
             self.__order_list.loc[index, 'SELL_PRICE'] = row['open']
-            self.__take_profit = None
+            self.__orders.clear()
 
     def __buy_limit(self, order: dict, row: pd.DataFrame, index: int):
         if order['price'] <= row['high'] and order['price'] >= row['low']:
             self.__position.increase(order['strategy'], 10)
             self.__order_list.loc[index, 'SIGNAL'] = order['signal']
             self.__order_list.loc[index, 'BUY_PRICE'] = order['price']
-            if 'take_profit' in order:
-                self.__take_profit = order['take_profit']
             self.__orders = list(filter(lambda item: item['id'] != order['id'], self.__orders))
+
+            if 'take_profit' in order:
+                self.create({'id': datetime.now().timestamp(), 'strategy': order['strategy'],
+                            'signal': 'TAKE_PROFIT', 'order': 'TAKE_PROFIT', 'price': order['take_profit']})
 
     def __sell_limit(self, order: dict, row: pd.DataFrame, index: int):
         if order['price'] <= row['high'] and order['price'] >= row['low']:
@@ -54,7 +49,18 @@ class Orders():
             self.__order_list.loc[index, 'SIGNAL'] = order['signal']
             self.__order_list.loc[index, 'SELL_PRICE'] = order['price']
             self.__orders = list(filter(lambda item: item['id'] != order['id'], self.__orders))
-            self.__take_profit = None
+
+            if self.__position.get_size(order['strategy']) == 0:
+                self.__orders = list(filter(lambda item: item['strategy'] != order['strategy'], self.__orders))
+
+    def __take_profit(self, order: dict, row: pd.DataFrame, index: int):
+        if order['price'] <= row['high'] and order['price'] >= row['low']:
+            self.__position.decrease(order['strategy'], 10)
+            self.__order_list.loc[index, 'SIGNAL'] = order['signal']
+            self.__order_list.loc[index, 'SELL_PRICE'] = order['price']
+            self.__orders = list(filter(lambda item: item['id'] != order['id'], self.__orders))
+        else:
+            self.__order_list.loc[index, 'TAKE_PROFIT'] = order['price']
 
     def get_order_list(self) -> pd.DataFrame:
         return self.__order_list
