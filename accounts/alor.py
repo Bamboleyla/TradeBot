3932591b2ff63886833769e25cc2deb0c7a20ff3
DVorobyov <dvorobjevredstar@mail.ru>
2024-12-25 19:52:01 +0500

    create live plot

--- accounts/alor.py
+++ accounts/alor.py
@@ -1,8 +1,7 @@
 import asyncio
 import pandas as pd
 import os
-import time
-
+import finplot as fplt
 from datetime import datetime, timezone, timedelta
 from api.client import AlorClientService
 from configurations.alor import AlorConfiguration
@@ -20,44 +19,62 @@ class AlorAccount:
         self.__orders = asyncio.run(self.__client.get_orders())
 
     def run(self):
-        loader = Downloader()
         if self.__config.is_work:
-
             manager = Manager('SBER')
             dir = manager.get_directory()
 
-            double_st = DoubleST(dir)
-
             data = pd.read_csv(os.path.join(dir, 'dobleST.csv'), header=0)
 
-            last_load_time = pd.to_datetime(data['date'].iloc[-1]).tz_localize('Etc/GMT-3')
-            time_until_next_update = None
+            def update():
+                loader = Downloader()
+                manager = Manager('SBER')
+                dir = manager.get_directory()
+                double_st = DoubleST(dir)
 
-            while self.__config.is_work:
-                time_now = datetime.now(timezone(timedelta(hours=3))).replace(microsecond=0)
+                data = pd.read_csv(os.path.join(dir, 'dobleST.csv'), header=0)
+                last_load_time = pd.to_datetime(data['date'].iloc[-1]).tz_localize('Etc/GMT-3')
+                time_until_next_update = None
 
-                if (last_load_time + timedelta(minutes=5)) < time_now:
-                    asyncio.run(loader.run(tickers=['SBER'], indexes=[]))
+                if self.__config.is_work:
+                    time_now = datetime.now(timezone(timedelta(hours=3))).replace(microsecond=0)
 
-                    quotes = manager.get_quotes()
-                    quotes['date'] = pd.to_datetime(quotes['date'], format='%Y%m%d %H:%M:%S')
+                    if (last_load_time + timedelta(minutes=5, seconds=20)) < time_now:
+                        asyncio.run(loader.run(tickers=['SBER'], indexes=[]))
 
-                    if data['date'].iloc[-1] != str(quotes['date'].iloc[-1]):
-                        data = double_st.run(quotes)
-                        data.to_csv(os.path.join(dir, 'dobleST.csv'), index=False)  # write data to file
+                        quotes = manager.get_quotes()
+                        quotes['date'] = pd.to_datetime(quotes['date'], format='%Y%m%d %H:%M:%S')
 
-                        last_load_time = pd.to_datetime(data['date'].iloc[-1]).tz_localize('Etc/GMT-3').replace(microsecond=0)
-                        time_until_next_update = None
+                        if data['date'].iloc[-1] != str(quotes['date'].iloc[-1]):
+                            data = double_st.run(quotes)
+                            data.to_csv(os.path.join(dir, 'dobleST.csv'), index=False)  # write data to file
 
-                    time.sleep(10)
+                            last_load_time = pd.to_datetime(data['date'].iloc[-1]).tz_localize('Etc/GMT-3').replace(microsecond=0)
+                            time_until_next_update = None
 
-                else:
-                    time_until_next_update = ((last_load_time + timedelta(minutes=5)) - time_now)
-                    info = pd.DataFrame(columns=['time_now', 'last_load_time', 'time_until_next_update'])
-                    info.loc[len(info)] = [time_now, last_load_time, time_until_next_update]
-                    print(info)
-                    time.sleep(10)
+                            # Update the live plot
+                            self.update_plot(data)
 
-        # data['date'] = pd.to_datetime(data['date'])  # Convert 'date' column to datetime type
-        # two_days_quotes = data[data['date'] >= data['date'].dt.strftime('%Y%m%d').unique()[-2] + ' 10:00:00']  # get last 2 days
-        # double_st.show(two_days_quotes)
+                    else:
+                        time_until_next_update = ((last_load_time + timedelta(minutes=5, seconds=20)) - time_now)
+                        info = pd.DataFrame(columns=['time_now', 'last_load_time', 'time_until_next_update'])
+                        info.loc[len(info)] = [time_now, last_load_time, time_until_next_update]
+                        print(info)
+
+            fplt.foreground = '#FFFFFF'
+            fplt.background = '#000000'
+            fplt.cross_hair_color = '#FFFFFF'
+
+            data.set_index('date', inplace=True)
+            data.index = pd.to_datetime(data.index).tz_localize('Etc/GMT-5')
+
+            fplt.candlestick_ochl(data[['open', 'close', 'high', 'low']].tail(100))
+            fplt.add_legend("SBER")
+            fplt.timer_callback(update, 10)  # start update timer
+            fplt.show()
+
+    def update_plot(self, data: pd.DataFrame):
+        data.set_index('date', inplace=True)
+        data.index = pd.to_datetime(data.index).tz_localize('Etc/GMT-5')
+
+        fplt.candlestick_ochl(data[['open', 'close', 'high', 'low']].tail(100))
+        fplt.refresh()
